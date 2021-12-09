d9b4089111491d675fb9f7e1bbbea0544f59352d
walter0514 <62457768+Wataru-Oshima-Tokyo@users.noreply.github.com>
2021-12-08 19:05:13 -0800

    Update face_recognition_ros.py

--- scripts/face_recognition_ros.py
+++ scripts/face_recognition_ros.py
@@ -3,7 +3,7 @@
 
 import cv2 
 import numpy as np
-from dlib import face_recognition as frec
+import dlib 
 from sensor_msgs.msg import Image
 import rospy
 import cv_bridge
@@ -14,9 +14,9 @@ class FACE():
 	def __init__(self):
 		print("__init__")
 		self.bridge = cv_bridge.CvBridge()
-		self.image_test= frec.load_image_file('/home/jetson/catkin_ws/src/face_recognition_ros/images/test.jpg')
+		self.image_test= face_recognition.load_image_file('/home/jetson/catkin_ws/src/face_recognition_ros/images/test.jpg')
 		# self.image_test=cv2.cvtColor(self.image_src, cv2.COLOR_BGR2GRAY)
-		self.encode_test=frec.face_encodings(self.image_test)[0]
+		self.encode_test=face_recognition.face_encodings(self.image_test)[0]
 		self.image_sub = rospy.Subscriber('/camera/color/image_raw', Image, self.image_callback)   #Image型で画像トピックを購読し，コールバック関数を呼ぶ
 		self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
 		self.twist = Twist()    #Twistインスタンス生成
@@ -24,7 +24,7 @@ class FACE():
 	def image_callback(self,msg):
 		self.image_src = self.bridge.imgmsg_to_cv2(msg, desired_encoding = 'bgr8')
 		## NEW ##
-		self.enocode_image = frec.face_encodings(self.image_src)[0]
+		self.enocode_image = face_recognition.face_encodings(self.image_src)[0]
 		self.recognize()
 		cv2.imshow("Result", self.image_src)
 		cv2.waitKey(3)
@@ -32,8 +32,8 @@ class FACE():
         def recognize(self):
  		try:
 			# self.image_src = cv2.cvtColor(self.image_src, cv2.COLOR_BGR2GRAY)
-			floc = frec.face_locations(self.image_src)[0]
-			self.encode_face = frec.face_encodings(self.image_src)[0]
+			floc = face_recognition.face_locations(self.image_src)[0]
+			self.encode_face = face_recognition.face_encodings(self.image_src)[0]
 			cv2.rectangle(self.image_src,(floc[3],floc[0]),(floc[1],floc[2]),(255,0,255),2)
 			self.compare()
 		except:
@@ -41,8 +41,8 @@ class FACE():
 			
 	def compare(self):
 		self.name = "Wataru"
-		self.result = frec.compare_faces([self.encode_face],self.encode_test)[0]
-		self.faceDis = frec.face_distance([self.encode_face],self.encode_test)[0]
+		self.result = face_recognition.compare_faces([self.encode_face],self.encode_test)[0]
+		self.faceDis = face_recognition.face_distance([self.encode_face],self.encode_test)[0]
 		print(self.result,self.faceDis)
 		# if self.faceDis <.45:
 		# 	cv2.putText(self.image_src, f'{self.name}{round(self.faceDis,2)}',(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
